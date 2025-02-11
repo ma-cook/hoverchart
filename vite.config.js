8d85e76407c4f3025965810f8f84bddf50751092
Mitchell <mitchell.cook@outlook.co.nz>
2025-02-11 23:59:13 +1300

    testing workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'three-vendor': ['three', 'three-stdlib'],
+          'react-vendor': ['react', 'react-dom'],
+          'firebase-vendor': [
+            'firebase/app',
+            'firebase/auth',
+            'firebase/firestore',
+          ],
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    exclude: ['three-stdlib'],
+  },
+});
