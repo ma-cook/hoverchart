7caebd918f2659dff87eee93177c550b8c050f0b
Mitchell <73150899+ma-cook@users.noreply.github.com>
2026-07-14 03:30:29 +1200

    Code: updated main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-// import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import AppShell from './AppShell.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './components/App';
+import './index.css'; // or global styles
 
-createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  <AppShell />
-  // </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
